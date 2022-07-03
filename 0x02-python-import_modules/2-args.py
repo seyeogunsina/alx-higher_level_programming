11a6d3d2b8f8d7ea29d3a728abd9ee2f20cfb813
seyeogunsina <adeseyeogunsina@gmail.com>
2022-07-03 14:03:25 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     print('{} argument'.format(len(argv) - 1), end='')
     if len(argv) == 1:
         print('s.')
