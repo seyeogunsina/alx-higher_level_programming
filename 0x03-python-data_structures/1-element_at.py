135e9ecb67d0f69329d32c641bb54b21e4cf8959
seyeogunsina <adeseyeogunsina@gmail.com>
2022-07-04 13:23:02 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,7 +5,4 @@ def element_at(my_list, idx):
     a function that retrieves an element from a list
     """
 
-    if (0 <= idx < len(my_list)):
-        return None
-
-    return my_list[idx]
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
